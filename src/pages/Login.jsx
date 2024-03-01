8a54065fd40b3ef17d3a72a2c07820919e3543cb
ARD-Code <andirajadewaa@gmail.com>
2024-03-02 04:30:33 +0700

    fixing typo on handlelogin function / adding login fiture

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,25 +4,41 @@ import { useEffect, useState } from "react";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [notif, setNotif] = useState("");
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
 
   const handleLogin = () => {
+    const payload = {
+      email: email,
+      password: password,
+    };
     axios
-      .get("https://reqres.in/api/login")
+      .post("https://reqres.in/api/login", payload)
       .then((res) => {
-        console.log(res);
+        setNotif("Login Berhasil");
+        const token = res?.data?.token;
+        localStorage.setItem("access_token", token);
       })
       .catch((err) => {
-        console.log(err);
+        setNotif(err?.response?.data?.error);
       });
   };
 
   return (
     <div>
       <h1 style={{ marginTop: "100px" }}>Login</h1>
-      <input type="email" placeholder="email" />
-      <input type="password" placeholder="password" />
+      {!!notif.length && <h1>{notif}</h1>}
+      <input type="email" placeholder="email" onChange={handleEmail} />
+      <input type="password" placeholder="password" onChange={handlePassword} />
       <div>
-        <button>Login</button>
+        <button onClick={handleLogin}>Login</button>
       </div>
     </div>
   );
