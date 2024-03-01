d38240fd90637197b1c60b6ce7b458131a09f3d0
ARD-Code <andirajadewaa@gmail.com>
2024-03-02 04:13:43 +0700

    initiate login page ( adding input element and create state for email and password)

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,29 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    axios
+      .get("https://reqres.in/api/login")
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div>
-      <h1 style={{ marginTop: "100px" }}>Login Page</h1>
+      <h1 style={{ marginTop: "100px" }}>Login</h1>
+      <input type="email" placeholder="email" />
+      <input type="password" placeholder="password" />
+      <div>
+        <button>Login</button>
+      </div>
     </div>
   );
 };
