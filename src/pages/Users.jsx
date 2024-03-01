943d980f85d4548281d91d54554e188835d3bdbb
ARD-Code <andirajadewaa@gmail.com>
2024-03-02 03:35:25 +0700

    adding users state and initiate getuserdata function

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,6 +1,26 @@
+import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
+import axios from "axios";
 
 const Users = () => {
+  const [users, setUsers] = useState();
+
+  const getUsersData = () => {
+    axios
+      .get("https://reqres.in/api/users?page=1")
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getUsersData();
+  }, []);
+
+  // console.log("data", users);
   return (
     <div>
       <Navbar />
