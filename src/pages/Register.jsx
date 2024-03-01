d84f1c32ce5506829bd0308c8a5cb6a778d44227
ARD-Code <andirajadewaa@gmail.com>
2024-03-02 04:39:16 +0700

    mirroring login sintaks for register page , adding adjusment to the api link

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,45 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [notif, setNotif] = useState("");
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleLogin = () => {
+    const payload = {
+      email: email,
+      password: password,
+    };
+    axios
+      .post("https://reqres.in/api/register", payload)
+      .then((res) => {
+        setNotif("Login Berhasil");
+        const token = res?.data?.token;
+        localStorage.setItem("access_token", token);
+      })
+      .catch((err) => {
+        setNotif(err?.response?.data?.error);
+      });
+  };
+
   return (
     <div>
-      <h1 style={{ marginTop: "100px" }}>This is a Register Page</h1>
+      <h1 style={{ marginTop: "100px" }}>Register</h1>
+      {!!notif.length && <h1>{notif}</h1>}
+      <input type="email" placeholder="email" onChange={handleEmail} />
+      <input type="password" placeholder="password" onChange={handlePassword} />
+      <div>
+        <button onClick={handleLogin}>Login</button>
+      </div>
     </div>
   );
 };
