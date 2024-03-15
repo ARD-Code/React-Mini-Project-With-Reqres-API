7caea24280f440aa92840b1d18a96047249a6f61
ARD-Code <andirajadewaa@gmail.com>
2024-03-15 22:02:12 +0700

    menambah pagination pada halaman users , agar bisa mengaksaes lebih dari 1 halaman

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,16 +1,19 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
 const Users = () => {
   const [users, setUsers] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
 
-  const getUsersData = () => {
+  const getUsersData = (pageNumber) => {
     axios
-      .get("https://reqres.in/api/users?page=1")
+      .get(`https://reqres.in/api/users?page=${pageNumber}`)
       .then((res) => {
         setUsers(res.data.data);
+        setTotalPages(res.data.total_pages);
       })
       .catch((err) => {
         console.log(err);
@@ -18,10 +21,25 @@ const Users = () => {
   };
 
   useEffect(() => {
-    getUsersData();
-  }, []);
+    getUsersData(page);
+  }, [page]);
 
-  console.log("data", users);
+  const nextPage = () => {
+    if (page < totalPages) {
+      setPage(page + 1);
+    }
+  };
+
+  const prevPage = () => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  };
+
+  const changePage = (pageNumber) => {
+    setPage(pageNumber);
+  };
+
   return (
     <div>
       <Navbar />
@@ -34,6 +52,22 @@ const Users = () => {
           </Link>
         </div>
       ))}
+      <div style={{ marginTop: "20px" }}>
+        <button onClick={prevPage} disabled={page === 1}>
+          Previous
+        </button>
+        {[...Array(totalPages).keys()].map((pageNumber) => (
+          <button
+            key={pageNumber + 1}
+            onClick={() => changePage(pageNumber + 1)}
+          >
+            {pageNumber + 1}
+          </button>
+        ))}
+        <button onClick={nextPage} disabled={page === totalPages}>
+          Next
+        </button>
+      </div>
     </div>
   );
 };
